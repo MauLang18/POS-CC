d79200e997d67b74a834e4dbc7572605eb6364e9
MauLang18 <maulangbonilla.18@gmail.com>
2024-11-24 11:29:55 -0600

    changes vault

--- POS.Api/Middleware/HealthCheckExtension.cs
+++ POS.Api/Middleware/HealthCheckExtension.cs
@@ -1,14 +1,28 @@
-﻿namespace POS.Api.Middleware;
+﻿using Newtonsoft.Json;
+using POS.Application.Commons.Config;
+using POS.Application.Interfaces.Services;
+
+namespace POS.Api.Middleware;
 
 public static class HealthCheckExtension
 {
     private static readonly string[] DatabaseTags = { "database" };
 
-    public static IServiceCollection AddHealthCheck(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddHealthCheck(this IServiceCollection services, IVaultSecretService vaultSecretService)
     {
+        var secretJson = vaultSecretService.GetSecret("CustomCodeAPI/data/ConnectionStrings").GetAwaiter().GetResult();
+        var secretResponse = JsonConvert.DeserializeObject<SecretResponse<ConnectionStringsConfig>>(secretJson);
+
+        if (secretResponse?.Data?.Data == null || string.IsNullOrEmpty(secretResponse.Data.Data.Connection))
+        {
+            throw new Exception("No se pudo obtener la cadena de conexión desde Vault.");
+        }
+
+        var connectionString = secretResponse.Data.Data.Connection;
+
         services.AddHealthChecks()
             .AddNpgSql(
-                configuration.GetConnectionString("Connection")!,
+                connectionString,
                 tags: DatabaseTags);
 
         services.AddHealthChecksUI()
